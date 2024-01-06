aacb033ded9fc186d80ad890358cff605af0b393
EngHeba1 <eheba0302@gmail.com>
2024-01-06 16:43:50 +0200

    creat clean arcture design

---
+++ lib/e_commerce.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ECommerce extends StatelessWidget {
+  const ECommerce({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'e_commerce.dart';
+
+void main() {
+  runApp(const ECommerce());
+}
+
+
